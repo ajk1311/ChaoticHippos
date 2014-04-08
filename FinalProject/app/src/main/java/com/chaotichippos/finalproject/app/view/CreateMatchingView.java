70948b8dbe733d54211cbccdf21041daf69e1289
naugust <naugust@umich.edu>
2014-04-07 21:51:05 -0400

    set matching items to deselect
    Deselects the list items on deletion of action mode if no deletion was
    made

--- FinalProject/app/src/main/java/com/chaotichippos/finalproject/app/view/CreateMatchingView.java
+++ FinalProject/app/src/main/java/com/chaotichippos/finalproject/app/view/CreateMatchingView.java
@@ -108,7 +108,11 @@ public class CreateMatchingView extends LinearLayout {
 
             @Override
             public void onDestroyActionMode(ActionMode actionMode) {
-
+                for(int i:selectedIndexes)
+                {
+                    matchItems.get(i).setSelected(false);
+                }
+                listAdapter.notifyDataSetChanged();
             }
         });
 

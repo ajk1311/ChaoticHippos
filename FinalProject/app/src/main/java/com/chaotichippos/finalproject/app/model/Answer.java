c9dade63051c8c56c669a21d8eae5928cfe70d2d
Anthony Kause <akause@barracudanetworks.com>
2014-04-16 20:00:24 -0400

    Added checking functionality to Answer class

--- FinalProject/app/src/main/java/com/chaotichippos/finalproject/app/model/Answer.java
+++ FinalProject/app/src/main/java/com/chaotichippos/finalproject/app/model/Answer.java
@@ -26,4 +26,41 @@ public class Answer {
 	public void setAnswerText(String answerText) {
 		mAnswerText = answerText;
 	}
+
+	public static float checkAnswer(Question question, Answer answer) {
+		switch (question.getType()) {
+			case TRUE_OR_FALSE:
+				return checkTrueFalseAnswer(question, answer);
+
+			case FILL_IN_THE_BLANK:
+				return checkFillInTheBlankAnswer(question, answer);
+
+			case MULTIPLE_CHOICE:
+				return checkMultipleChoiceAnswer(question, answer);
+
+			case MATCHING:
+				return checkMatchingAnswer(question, answer);
+
+			default:
+				throw new IllegalArgumentException("Trying to check question of unknown type");
+		}
+	}
+
+	private static float checkTrueFalseAnswer(Question question, Answer answer) {
+		int provided = Integer.parseInt(answer.getAnswerText());
+		int correct = new TrueOrFalseQuestionWrapper(question).getAnswer();
+		return provided == correct ? 1 : 0;
+	}
+
+	private static float checkFillInTheBlankAnswer(Question question, Answer answer) {
+		return 0.0f;
+	}
+
+	private static float checkMultipleChoiceAnswer(Question question, Answer answer) {
+		return 0.0f;
+	}
+
+	private static float checkMatchingAnswer(Question question, Answer answer) {
+		return 0.0f;
+	}
 }

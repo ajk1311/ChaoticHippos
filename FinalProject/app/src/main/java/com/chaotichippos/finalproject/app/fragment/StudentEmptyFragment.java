8104ca404747941e1bec44b2a2567847e16c9386
Anthony Kause <akause@barracudanetworks.com>
2014-04-18 15:00:23 -0400

    Added switch menu option to empty veiw

--- FinalProject/app/src/main/java/com/chaotichippos/finalproject/app/fragment/StudentEmptyFragment.java
+++ FinalProject/app/src/main/java/com/chaotichippos/finalproject/app/fragment/StudentEmptyFragment.java
@@ -2,12 +2,17 @@ package com.chaotichippos.finalproject.app.fragment;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.chaotichippos.finalproject.app.R;
+import com.chaotichippos.finalproject.app.activity.InstructorActivity;
 import com.chaotichippos.finalproject.app.activity.MainActivity;
 
 public class StudentEmptyFragment extends Fragment {
@@ -25,11 +30,30 @@ public class StudentEmptyFragment extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
 		mMainActivity.getMainPane().closePane();
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_student_empty, container, false);
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		super.onCreateOptionsMenu(menu, inflater);
+		menu.add(Menu.NONE, R.id.menu_option_switch, Menu.NONE, "Switch to Instructor")
+				.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.menu_option_switch:
+				startActivity(new Intent(getActivity(), InstructorActivity.class)
+						.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK));
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
